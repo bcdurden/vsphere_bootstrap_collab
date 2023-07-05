e8741f624299a354502516a39aae79c37ae4fea5
Brian Durden <brian.durden@rancherfederal.com>
2023-07-05 17:14:49 -0400

    suse terraform

---
+++ terraform/suse/jumpbox.tf
@@ -0,0 +1,45 @@
+resource "vsphere_virtual_machine" "jumpbox" {
+  name                 = "jumpbox-suse"
+  datastore_id         = data.vsphere_datastore.datastore.id
+  host_system_id       = data.vsphere_host.host.id
+  resource_pool_id     = data.vsphere_compute_cluster.cluster.resource_pool_id
+  folder               = var.vm_folder
+
+  wait_for_guest_net_timeout = 5
+
+  num_cpus         = 4
+  memory           = 8192
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  clone {
+    template_uuid = data.vsphere_content_library_item.vm_ovf.id
+  }
+
+  disk {
+    label            = "disk0"
+    size             = 120
+  }
+  cdrom {
+    client_device = true
+  }
+  extra_config = {
+      "guestinfo.userdata"          = base64encode( <<EOT
+        #cloud-config
+        package_update: true
+        hostname: jumpbox-suse
+        password: supersecretpassword
+        chpasswd: { expire: False }
+        ssh_pwauth: True
+        packages: []
+        runcmd: []
+        ssh_authorized_keys: 
+        - ${tls_private_key.global_key.public_key_openssh}
+      EOT
+      )  
+      "guestinfo.userdata.encoding" = "base64"
+      "guestinfo.metadata"          = ""
+      "guestinfo.metadata.encoding" = "base64"
+    }
+}
---
+++ terraform/suse/variables.tf
@@ -0,0 +1,62 @@
+variable "vsphere_user" {
+    type = string
+    description = "Username of vsphere environment"
+}
+
+variable "vsphere_password" {
+    type = string
+    description = "Password for username in vsphere environment"
+    default = "CT0R0ad$how!"
+}
+
+variable "vsphere_server" {
+    type = string
+    description = "Server URL for vsphere environment"
+}
+
+variable "skip_ssl_verify" {
+    type = bool
+    description = "Flag to disable SSL verification when connecting to vsphere"
+    default = true
+}
+
+variable "datacenter_name" {
+    type = string
+    description = "DC name in vsphere"
+}
+
+variable "datastore_name" {
+    type = string
+    description = "Datastore name"
+}
+
+variable "cluster_name" {
+    type = string
+    description = "Cluster name in vsphere"
+}
+
+variable "content_library_name" {
+    type = string
+    description = "The content library name hosting the vm ovf/ova"
+}
+
+variable "network_name" {
+    type = string
+    description = "Network name for cluster"
+}
+
+variable "vm_image_name" {
+    type = string
+    description = "The name of the image in your content library"
+}
+
+variable "esxi_hosts" {
+    type = list
+    description = "List of ESXi hosts"
+}
+
+variable "vm_folder" {
+  type        = string
+  description = "The vSphere VM folder in which to create the cluster"
+  default     = "rancher"
+}
